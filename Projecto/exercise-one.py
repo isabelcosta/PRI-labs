84d21e7dc433a0c7723174a655abbcb62e57a056
PTRevolution <fernando9b@hotmail.com>
2016-10-24 00:57:33 +0100

    get document content and stop words

--- Projecto/exercise-one.py
+++ Projecto/exercise-one.py
@@ -1,5 +1,8 @@
 from __future__ import division, unicode_literals
 from textblob import TextBlob
+
+from nltk.corpus import stopwords
+
 from sklearn.datasets import fetch_20newsgroups
 train = fetch_20newsgroups(subset='train')
 test = fetch_20newsgroups(subset='test')
@@ -7,41 +10,52 @@ test = fetch_20newsgroups(subset='test')
 import math
 import os
 
+i = 1
+fileList = []
+documentList = []
 
-listaDeFicheiros = []
-
-for file in os.listdir("C:/Users/Fernando/Desktop/fao30/documents"):
+for file in os.listdir("C:/Users/Fernando/Desktop/PRI/PRI-labs/Projecto/fao30/documents"):
     if file.endswith(".txt"):
-        listaDeFicheiros += [file]
-
-print listaDeFicheiros
-
-def tf(word, blob):
-    return blob.words.count(word) / len(blob.words)
-
-def n_containing(word, bloblist):
-    return sum(1 for blob in bloblist if word in blob.words)
-
-def idf(word, bloblist):
-    return math.log(len(bloblist) / (1 + n_containing(word, bloblist)))
-
-def tfidf(word, blob, bloblist):
-    return tf(word, blob) * idf(word, bloblist)
+        f = open("C:/Users/Fernando/Desktop/PRI/PRI-labs/Projecto/fao30/documents/" + file, 'r')
+        content = f.read()
+        fileList += [content]
+        f.close()
 
 
-blob1 = TextBlob(train.data[1])
-blob2 = TextBlob(train.data[2])
-blob3 = TextBlob(train.data[3])
+
+for file in fileList:
+    docContent = TextBlob(file.decode('unicode_escape'))
+    documentList += [docContent]
 
 
-bloblist = [blob1, blob2, blob3]
 
-# print "imprimes ?"
-#print test.noun_phrases
+def tf(word, docContent):
+    return docContent.words.count(word) / len(docContent.words)
+
+def n_containing(word, documentList):
+    return sum(1 for docContent in documentList if word in docContent.words)
+
+def idf(word, documentList):
+    return math.log(len(documentList) / (1 + n_containing(word, documentList)))
+
+def tfidf(word, docContent, documentList):
+    return tf(word, docContent) * idf(word, documentList)
 
 
-# print tf("guy",blob1)
+
+doc1 = TextBlob("ola, adeus,!!! the")
+doc2 = TextBlob(train.data[2])
+doc3 = TextBlob(train.data[3])
+
+
+documentList = [doc1, doc2, doc3]
+
+# stop_words = set(stopwords.words('english'))
+# doc1 = set(doc1.words) - stop_words
 #
-# print idf("guy", bloblist)
+# print doc1
 #
-# print blob1.ngrams(n=2)
+# print idf("guy", documentFist)
+#
+# print doc1.ngrams(n=2)
+
