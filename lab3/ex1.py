c01527ef30d6ece041898885a0e7f77062448e30
Isabel Costa <isabelcmdcosta@gmail.com>
2016-10-23 18:41:14 +0100

    lab3 ex1

---
+++ lab3/ex1.py
@@ -0,0 +1,16 @@
+from sklearn.datasets import fetch_20newsgroups
+train = fetch_20newsgroups(subset='train')
+test = fetch_20newsgroups(subset='test')
+
+print train.data[:10]
+print train.target[:10]
+print train.target_names
+
+from sklearn.feature_extraction.text import TfidfVectorizer
+vectorizer = TfidfVectorizer(use_idf=False)
+trainvec = vectorizer.fit_transform(train.data)
+testvec = vectorizer.transform(test.data)
+
+print vectorizer
+print trainvec
+print testvec
