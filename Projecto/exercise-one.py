0a191b2759c521d54006cfa578724454c44b72e7
PTRevolution <fernando9b@hotmail.com>
2016-10-23 22:13:18 +0100

    actualizacao do ex 1

--- Projecto/exercise-one.py
+++ Projecto/exercise-one.py
@@ -1,13 +1,18 @@
 from __future__ import division, unicode_literals
 from textblob import TextBlob
-
-import nltk
-import math
-
-
 from sklearn.datasets import fetch_20newsgroups
 train = fetch_20newsgroups(subset='train')
 test = fetch_20newsgroups(subset='test')
+
+import math
+import os
+
+
+listaDeFicheiros = []
+
+for file in os.listdir("C:/Users/Fernando/Desktop/fao30/documents"):
+    if file.endswith(".txt"):
+        listaDeFicheiros += [file]
 
 
 def tf(word, blob):
@@ -23,22 +28,19 @@ def tfidf(word, blob, bloblist):
     return tf(word, blob) * idf(word, bloblist)
 
 
-print "\n" + train.data[1]
-
-# listaPalavras = []
-
-#for sentence in nltk.sent_tokenize(train.data[1]):
-#    listaPalavras = nltk.word_tokenize(sentence)
-#    print nltk.pos_tag(listaPalavras)
-
 blob1 = TextBlob(train.data[1])
 blob2 = TextBlob(train.data[2])
 blob3 = TextBlob(train.data[3])
+test = TextBlob("ola ola adeus")
 
 bloblist = [blob1, blob2, blob3]
 
-print tf("guy",blob1)
+# print "imprimes ?"
+print test.noun_phrases
 
-print idf("guy", bloblist)
 
-print blob1.ngrams(n=2)
+# print tf("guy",blob1)
+#
+# print idf("guy", bloblist)
+#
+# print blob1.ngrams(n=2)
