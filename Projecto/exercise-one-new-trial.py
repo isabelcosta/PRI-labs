ffbf1d70001e9c07a7d4bf110b795e1ec4433b59
PTRevolution <fernando9b@hotmail.com>
2016-10-31 19:31:40 +0000

    fix exercice 1 new trial : words

--- Projecto/exercise-one-new-trial.py
+++ Projecto/exercise-one-new-trial.py
@@ -8,9 +8,24 @@ import os
 """ Functions """
 
 def tf(word, docContent):
+    if " " in word:
+        docContentAux = []
+        for bigram in docContent.ngrams(n=2):
+            docContentAux += [" ".join(bigram)]
+        return docContentAux.count(word) / len(docContentAux)
+
     return docContent.words.count(word) / len(docContent.words)
 
 def n_containing(word, documentList):
+    if " " in word:
+        sumDoc = 0
+        for docContent in documentList:
+            docContentAux = []
+            for bigram in TextBlob(docContent).ngrams(n=2):
+                docContentAux += [" ".join(bigram)]
+            if word in docContentAux:
+                sumDoc += 1
+        return sumDoc
     return sum(1 for docContent in documentList if word in TextBlob(docContent).words)
 
 def idf(word, documentList):
@@ -37,6 +52,7 @@ docCandidates = []
 train = fetch_20newsgroups(subset='train');
 trainData = train.data[:10]
 
+
 # Use english text document to apply keyphrase extraction method
 f = open("candidates.txt", 'r')
 fileContent = f.read().decode('unicode_escape')
@@ -46,11 +62,11 @@ print fileContent
 
 docContentBlob = TextBlob(fileContent)
 
-stopWords = set(stopwords.words('english'))
+stopWords = list(stopwords.words('english'))
 
 # remove stop words from each document
 # [word1, word2, ...] without stop words
-docWithoutStopWords = list(set(docContentBlob.words) - stopWords)
+docWithoutStopWords = [x for x in docContentBlob.words if x not in stopWords]
 
 # joins all words from a document separated by spaces, without the stopwords, to crate a TextBlob
 documentSentence = " ".join(docWithoutStopWords)
@@ -83,7 +99,32 @@ print docCandidates
 #         f.close()
 #
 
-# # create dictionary with words/bi-grams and it's score
+
+
+# ----------------------------------------------------------------------------------
+
+#FIXME tf-idf for words seems to be working, WIP in bigrams
+#create dictionary with words/bi-grams and it's score
+for pos, trainDoc in enumerate(trainData):
+    # dictionary containing word/bi-grams and its scores of tf_idf{ word: tf_idf } for a certain document
+    keyphrasesDict = {}
+
+
+    # /// Create 2 diferent lists (words, bigrams) for each document and the candidates doc ////
+
+
+    print "\nDoc " + str(pos) + " ------------------------------\n"
+    print trainDoc
+    print docCandidates
+    for word in docCandidates:
+
+        trainBlob = TextBlob(trainDoc.decode('unicode_escape'))
+        keyphrasesDict[word] = tfidf(word, trainBlob, trainData)
+
+        print "Word: " + word + "     TF-IDF: " + str(keyphrasesDict[word])
+    docCandidatesScored += [keyphrasesDict]
+
+# create dictionary with words/bi-grams and it's score
 # for doc in fileList:
 #     # dictionary containing word/bi-grams and its scores of tf_idf{ word: tf_idf } for a certain document
 #     keyphrasesDict = {}
@@ -94,24 +135,3 @@ print docCandidates
 #     docCandidatesScored += [keyphrasesDict]
 #
 # print docCandidatesScored
-
-# ----------------------------------------------------------------------------------
-
-# FIXME tf-idf calculations not working
-# create dictionary with words/bi-grams and it's score
-for pos, trainDoc in enumerate(trainData):
-    # dictionary containing word/bi-grams and its scores of tf_idf{ word: tf_idf } for a certain document
-    keyphrasesDict = {}
-
-    print "\nDoc " + str(pos) + " ------------------------------\n"
-
-    for word in docCandidates:
-        trainBlob = TextBlob(trainDoc.decode('unicode_escape'))
-        keyphrasesDict[word] = tfidf(word, trainBlob, trainData)
-        # print "-----------////////////------------"
-        # print trainDoc
-        print "Word: " + word + "     TF-IDF: " + str(keyphrasesDict[word])
-        # print "-----------////////////------------"
-    docCandidatesScored += [keyphrasesDict]
-
-#print docCandidatesScored
