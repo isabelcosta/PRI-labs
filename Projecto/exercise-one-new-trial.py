9d669be0129bfc36f194c372a84887743a10d322
PTRevolution <fernando9b@hotmail.com>
2016-11-01 18:39:17 +0000

    doc collection division by word and bigrams | added more candidates

--- Projecto/exercise-one-new-trial.py
+++ Projecto/exercise-one-new-trial.py
@@ -2,44 +2,56 @@ from __future__ import division, unicode_literals
 from sklearn.datasets import fetch_20newsgroups
 from textblob import TextBlob
 from nltk.corpus import stopwords
+from nltk import ngrams
 import math
-import os
+import operator
 
-""" Functions """
+
+# ---------------""" Functions for WORDS """--------------------- #
 
 def tf(word, docContent):
-    if " " in word:
-        docContentAux = []
-        for bigram in docContent.ngrams(n=2):
-            docContentAux += [" ".join(bigram)]
-        return docContentAux.count(word) / len(docContentAux)
-
-    return docContent.words.count(word) / len(docContent.words)
+    # word -> candidate (string)
+    # docContent -> list of document words without stop words(list)
+    return docContent.count(word) / len(docContent)
 
 def n_containing(word, documentList):
-    if " " in word:
-        sumDoc = 0
-        for docContent in documentList:
-            docContentAux = []
-            for bigram in TextBlob(docContent).ngrams(n=2):
-                docContentAux += [" ".join(bigram)]
-            if word in docContentAux:
-                sumDoc += 1
-        return sumDoc
-    return sum(1 for docContent in documentList if word in TextBlob(docContent).words)
+    return sum(1 for docContent in documentList if word in docContent)
 
 def idf(word, documentList):
-    return math.log(len(documentList) / (1 + n_containing(word, documentList)))
+    return math.log((len(documentList) +1) / (1 + n_containing(word, documentList)))
 
-# docContent is TextBlob
-# documentList is a list with all documents
 def tfidf(word, docContent, documentList):
     return tf(word, docContent) * idf(word, documentList)
+
+# ----------------""" Functions for BIGRAMS """-------------------- #
+
+def tfForBigram(word, docContent):
+    docContentAux = []
+    for bigram in docContent.ngrams(n=2):
+        docContentAux += [" ".join(bigram)]
+    return docContentAux.count(word) / len(docContentAux)
+
+def n_containingForBigram(word, documentList):
+    sumDoc = 0
+    for docContent in documentList:
+        docContentAux = []
+        for bigram in TextBlob(docContent).ngrams(n=2):
+            docContentAux += [" ".join(bigram)]
+        if word in docContentAux:
+            sumDoc += 1
+    return sumDoc
+
+def idfForBigram(word, documentList):
+    return math.log((len(documentList)+1) / (1 + n_containingForBigram(word, documentList)))
+
+def tfidfForBigram(word, docContent, documentList):
+    return tfForBigram(word, docContent) * idfForBigram(word, documentList)
+
 
 
 """ Script """
 
-fileList = []
+result = []
 documentWordList = []
 documentBiGramList = []
 documentBiGramWordList = []
@@ -50,7 +62,7 @@ docCandidatesScored = []
 docCandidates = []
 
 train = fetch_20newsgroups(subset='train');
-trainData = train.data[:10]
+trainData = train.data[:9]
 
 
 # Use english text document to apply keyphrase extraction method
@@ -68,6 +80,29 @@ stopWords = list(stopwords.words('english'))
 # [word1, word2, ...] without stop words
 docWithoutStopWords = [x for x in docContentBlob.words if x not in stopWords]
 
+
+finalDocWordList = []
+finalDocList = []
+for trainDoc in trainData:
+    trainBlob = TextBlob(trainDoc.decode('utf-8'))
+    trainBlobWithoutStopWords = [x for x in trainBlob.words if x not in stopWords]
+    finalDocWordList += [trainBlobWithoutStopWords]
+
+finalDocList += finalDocWordList
+finalDocBigramList = []
+for num,trainDoc in enumerate(finalDocWordList):
+    docContentAux = []
+    DocBigramList = []
+    docContentAux = ngrams(trainDoc, 2)
+    for gram in docContentAux:
+        DocBigramList += [" ".join(list(gram))]
+    finalDocList[num] += DocBigramList
+
+
+print finalDocList
+
+
+
 # joins all words from a document separated by spaces, without the stopwords, to crate a TextBlob
 documentSentence = " ".join(docWithoutStopWords)
 
@@ -79,10 +114,83 @@ documentBiGramList = []
 for word in docContentBlobWithoutStopWords.ngrams(n=2):
     documentBiGramList += [" ".join(word)]
 
-# joins all words and bi-grams from a document
-docCandidates += docWithoutStopWords + documentBiGramList
+print documentBiGramList
 
-print docCandidates
+# joins all words and bi-grams from a document
+docFinalCandidates = docWithoutStopWords + documentBiGramList
+
+
+
+
+
+
+
+#FIXME tf-idf for words seems to be working, WIP in bigrams
+#create dictionary with words/bi-grams and it's score
+for pos, trainDoc in enumerate(finalDocList):
+    # dictionary containing word/bi-grams and its scores of tf_idf{ word: tf_idf } for a certain document
+    keyphrasesDict = {}
+    # /// Create 2 diferent lists (words, bigrams) for each document and the candidates doc ////
+
+    print "\nDoc " + str(pos) + " ------------------------------\n"
+    print "finalCandidates: " + str(docFinalCandidates)
+    print " \n "
+
+    for word in docFinalCandidates:
+        #trainBlob = TextBlob(trainDoc.decode('utf-8'))
+        keyphrasesDict[word] = tfidf(word, trainDoc, finalDocList)
+        print "Word: " + word + " ---- " + str(keyphrasesDict[word])
+
+    docCandidatesScored += [keyphrasesDict]
+
+print " \n "
+
+for doc,candidateScored in enumerate(docCandidatesScored):
+    print "Document " + str(doc) + " --- " + str(candidateScored) + "\n"
+
+
+
+
+
+
+
+
+
+# create dictionary with words/bi-grams and it's score
+# for doc in fileList:
+#     # dictionary containing word/bi-grams and its scores of tf_idf{ word: tf_idf } for a certain document
+#     keyphrasesDict = {}
+#     for word in docCandidates:
+#         keyphrasesDict[word] = tfidf(word, TextBlob(doc.decode('unicode_escape')), fileList)
+#         print keyphrasesDict[word]
+#         print word
+#     docCandidatesScored += [keyphrasesDict]
+#
+# print docCandidatesScored
+
+
+# def tf(word, docContent):
+#     if " " in word:
+#         docContentAux = []
+#         for bigram in docContent.ngrams(n=2):
+#             docContentAux += [" ".join(bigram)]
+#         return docContentAux.count(word) / len(docContentAux)
+#
+#     return docContent.words.count(word) / len(docContent.words)
+#
+# def n_containing(word, documentList):
+#     if " " in word:
+#         sumDoc = 0
+#         for docContent in documentList:
+#             docContentAux = []
+#             for bigram in TextBlob(docContent).ngrams(n=2):
+#                 docContentAux += [" ".join(bigram)]
+#             if word in docContentAux:
+#                 sumDoc += 1
+#         return sumDoc
+#     return sum(1 for docContent in documentList if word in TextBlob(docContent).words)
+#
+#
 
 # ----------------------------------------------------------------------------------
 # Get relative path to documents
@@ -99,39 +207,4 @@ print docCandidates
 #         f.close()
 #
 
-
-
 # ----------------------------------------------------------------------------------
-
-#FIXME tf-idf for words seems to be working, WIP in bigrams
-#create dictionary with words/bi-grams and it's score
-for pos, trainDoc in enumerate(trainData):
-    # dictionary containing word/bi-grams and its scores of tf_idf{ word: tf_idf } for a certain document
-    keyphrasesDict = {}
-
-
-    # /// Create 2 diferent lists (words, bigrams) for each document and the candidates doc ////
-
-
-    print "\nDoc " + str(pos) + " ------------------------------\n"
-    print trainDoc
-    print docCandidates
-    for word in docCandidates:
-
-        trainBlob = TextBlob(trainDoc.decode('unicode_escape'))
-        keyphrasesDict[word] = tfidf(word, trainBlob, trainData)
-
-        print "Word: " + word + "     TF-IDF: " + str(keyphrasesDict[word])
-    docCandidatesScored += [keyphrasesDict]
-
-# create dictionary with words/bi-grams and it's score
-# for doc in fileList:
-#     # dictionary containing word/bi-grams and its scores of tf_idf{ word: tf_idf } for a certain document
-#     keyphrasesDict = {}
-#     for word in docCandidates:
-#         keyphrasesDict[word] = tfidf(word, TextBlob(doc.decode('unicode_escape')), fileList)
-#         print keyphrasesDict[word]
-#         print word
-#     docCandidatesScored += [keyphrasesDict]
-#
-# print docCandidatesScored
