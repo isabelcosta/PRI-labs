4bfea3936bbe84f53bdac049d5ddd61f7fc40dd4
marinaferreira9 <msf923@gmail.com>
2016-12-10 00:45:48 +0000

    ex2

--- Projecto2/ex2.py
+++ Projecto2/ex2.py
@@ -19,48 +19,31 @@ stopWords = list(stopwords.words('english'))
 
 ### Mean Precision
 
-def calc_precision(tp, fp):
-    return float(tp / (tp + fp))
-
-def calc_recall(tp, fn):
-    return float(tp / (tp + fn))
-
-def calc_f1(precision, recall):
-    if precision == 0 and recall == 0:
-        return "N/A"
-    return 2 * ((precision * recall) / (precision + recall))
-
-def calc_precision_recall_f1_score(docName, knownKeyphrases, predictedKeyphrases):
-
-    tp = sum(1 for key in knownKeyphrases if key in predictedKeyphrases)
-    # tp = sum(1 for key in predictedKeyphrases if key in knownKeyphrases)
-
-    fn = len(knownKeyphrases) - tp
-    fp = len(predictedKeyphrases) - tp
-
-    precision = calc_precision(tp, fp)
-    recall = calc_recall(tp, fn)
-
-    f1_score = calc_f1(precision, recall)
-
-    print docName + ": " + str(precision) + ", " + str(recall) + ", " + str(f1_score)
-    return precision, recall, f1_score
-
 def calc_mean_avg_precision(all_precisions):
     mean_avg_precision =  sum(all_precisions)/len(all_precisions)
     print "Mean Average Precision: \t" + str(mean_avg_precision)
     return mean_avg_precision
 
 
+def precisonCalc(knownKeyphrases, predictedKeyphrases):
+    tp = sum(1 for key in knownKeyphrases if key in predictedKeyphrases)
+    fp = len(predictedKeyphrases) - tp
+
+    return float(tp / (tp + fp))
+
+
+
 #CALCULO IDF
 # N="docs in collection, nt=#docs with term
 def IDF(candidatesByDoc, filteredTrain, NDocs):
+    print "idf"
     scoresIDF = {}
     for doc in candidatesByDoc:
+        print doc
         docScores={}
         for ngram in candidatesByDoc[doc]:
             #print ngram
-            nt = sum(1 for doc in filteredTrain if ngram in filteredTrain[doc]) #FALTA FILTERING TRAIN DATASET
+            nt = sum(1 for doc1 in filteredTrain if ngram in filteredTrain[doc]) #FALTA FILTERING TRAIN DATASET
             #print NDocs
             #print nt
             docScores[ngram] = log(1+(NDocs-nt+0.5)/(nt+0.5))
@@ -90,14 +73,6 @@ def BM25( idf, tf, D, avgdl):
     bottom = tf + k * (1 - b + b * aux)
     return idf * top/bottom
 
-
-### PRIOR
-#def Prior():
-
-
-
-
-### WEIGHT
 
 
 """
@@ -185,13 +160,12 @@ def pagerankImproved(graph, keywords, Prior, d, numloops):
 
 
 
-
 def createGraph(DocCandidates, candidatesBySentences):
 
     graph={}
 
     for ngram in DocCandidates:
-        #print ngram
+
         co_oc={}
 
         for sentence in candidatesBySentences:
@@ -229,38 +203,16 @@ def extract_candidate_chunks(text, grammar=r'KT: {(<JJ>* <NN.*>+ <IN>)? <JJ>* <N
     return [cand for cand in candidates]
 
 
-
-def wordToNgrams(text, n, exact=True):
-    return [" ".join(j) for j in zip(*[text[i:] for i in range(n)])]
-
-
-def ngrams(text):
-
-    finalCandidates = []
-
-    text = "".join([c for c in text if c not in string.punctuation])
-    text = ''.join([c for c in text if not c.isdigit()]).lower()
-    tokensWithoutStopWords = [x for x in text.split() if x not in stopWords]
-
-    finalCandidates += tokensWithoutStopWords
-    finalCandidates += wordToNgrams(tokensWithoutStopWords, 2)
-    finalCandidates += wordToNgrams(tokensWithoutStopWords, 3)
-
-   # print finalCandidates
-
-    return finalCandidates
-
-
 ###
 
 
 
 def tok(text):
 
-    text = "".join([c for c in text if c not in string.punctuation])
-    text = ''.join([c for c in text if not c.isdigit()])
-    tokens = nltk.word_tokenize(text)
-    tokensWithoutStopWords = [x for x in tokens if x not in stopWords]
+    #text = "".join([c for c in text if c not in string.punctuation])
+    #text = ''.join([c for c in text if not c.isdigit()])
+    tokensWithoutStopWords = nltk.word_tokenize(text)
+    #tokensWithoutStopWords = [x for x in tokens if x not in stopWords]
 
     tags = nltk.pos_tag(tokensWithoutStopWords)
     namedEntities = nltk.ne_chunk(tags, binary=True)
@@ -292,10 +244,6 @@ def tok_sent(text):
         text="".join([c for c in sentence if c not in string.punctuation])
         text = ''.join([c for c in text if not c.isdigit()])
         sentenceList[i] =text
-
-
-    # for sent in sentList:
-    #      print sent
 
     return sentenceList
 
@@ -329,6 +277,7 @@ def extractKeyphrases( train, dataset):
         candidatesDoc = []
 
         # GET SENTENCES
+
         docSentences[doc] = tok_sent(dataset[doc])
 
         nSentences= len(docSentences[doc])
@@ -338,8 +287,10 @@ def extractKeyphrases( train, dataset):
         lengthDoc=0
 
         # GET NGRAMS
+
         for i,sentence in enumerate(docSentences[doc]):
             candidates = []
+            #print i
             candidates += tok(sentence)
 
 
@@ -352,10 +303,17 @@ def extractKeyphrases( train, dataset):
             candidatesBySentence[doc] += [candidates]
 
             # FOR EACH DOC
+           # print "ngrama and weight"
             for ngram in candidates:
                 if ngram not in candidatesDoc:
                     candidatesDoc.append(ngram)
                     ngram_pos[ngram]=(nSentences - i)/nSentences
+                # candidatesDoc.append(ngram)
+                # ngram_pos[ngram] = (nSentences - i) / nSentences
+
+
+
+
 
         # print candidatesBySentence
         docsLen[doc]= lengthDoc
@@ -369,8 +327,10 @@ def extractKeyphrases( train, dataset):
 
 
     #CREATE GRAPHS
+    print "Creating graphs.."
     graphs={}
     for doc in candidatesByDoc:
+        print doc
         graph={}
         graph= createGraph(candidatesByDoc[doc], candidatesBySentence[doc])
         graphs[doc]= graph
@@ -385,11 +345,8 @@ def extractKeyphrases( train, dataset):
 
     # CALCULATE PRIORS
 
-    print "YEYY TF"
+    print "TF and IDF calculations"
     scoresTF = TF(candidatesByDoc, filteredTrain, docsLen)
-
-
-    print "YEYY IDF"
     scoresIDF = IDF(candidatesByDoc, filteredTrain, NDocs)
 
 
@@ -400,7 +357,7 @@ def extractKeyphrases( train, dataset):
         sumWords += docsLen[doc]
     avgDL = sumWords / NDocs
 
-    print "YEYY BM25"
+    print "BM25 calculation"
     ###Calculating BM25 --> PRIOR
     scoresBM25 = {}
     for doc in candidatesByDoc:
@@ -422,60 +379,33 @@ def extractKeyphrases( train, dataset):
 
 
 
-
-
     ##CALCULATE PAGERANK
-    PageRankOriginal={}
-    PageRankImproved = {}
+    ## PageRank= [doc][type PageRank][keyphrases]
+    PageRank={}
+    print "PAGERANKS"
     for doc in candidatesByDoc:
+        prDoc={}
         print doc
+
+       # print "PAGERANK ORIGINAL"
         pro=pagerank(graphs[doc],   0.15, 3)
-        print "PAGERANK ORIGINAL"
-        print sorted(pro.iteritems(), key=operator.itemgetter(1), reverse=True)
-        PageRankOriginal[doc] = pro
+        #print sorted(pro.iteritems(), key=operator.itemgetter(1), reverse=True)
+        prDoc['ORIGINAL'] = sorted(pro.iteritems(), key=operator.itemgetter(1), reverse=True)[:5]
 
-        print "PAGERANK IMPROVED -  SENTENCE AND CO-OCCURRENCE"
+       # print "PAGERANK IMPROVED -  SENTENCE AND CO-OCCURRENCE"
         prISC = pagerankImproved(graphs[doc], candidatesByDoc[doc], Priors['BM25'][doc], 0.15, 3)
-        print sorted(prISC.iteritems(), key=operator.itemgetter(1), reverse=True)
-        #PageRankImproved[doc] = pri
+        #print sorted(prISC.iteritems(), key=operator.itemgetter(1), reverse=True)
+        prDoc['IMPROVED - SENTENCE PRIOR AND CO-OCCURENCE WEIGHT'] = sorted(prISC.iteritems(), key=operator.itemgetter(1), reverse=True)[:5]
 
-        print "PAGERANK IMPROVED -  BM25 AND CO-OCCURRENCE"
+       # print "PAGERANK IMPROVED -  BM25 AND CO-OCCURRENCE"
         prIBC = pagerankImproved(graphs[doc], candidatesByDoc[doc], Priors['SENTENCE'][doc], 0.15, 3)
-        print sorted(prIBC.iteritems(), key=operator.itemgetter(1), reverse=True)
+       # print sorted(prIBC.iteritems(), key=operator.itemgetter(1), reverse=True)
+        prDoc['IMPROVED - BM25 PRIOR AND CO-OCCURENCE WEIGHT'] = sorted(prIBC.iteritems(), key=operator.itemgetter(1), reverse=True)[:5]
 
-        # print "PAGERANK IMPROVED -  SENTENCE AND SIMILIRATY"
-        # prISS = pagerankImproved(graphs[doc], candidatesByDoc[doc], Priors['BM25'], 0.15, 3)
-        # print sorted(prISS.iteritems(), key=operator.itemgetter(1), reverse=True)
-        #
-        # print "PAGERANK IMPROVED -  SENTENCE AND CO-OCCURRENCE"
-        # prIBS = pagerankImproved(graphs[doc], candidatesByDoc[doc], Priors['BM25'], 0.15, 3)
-        # print sorted(prIBS.iteritems(), key=operator.itemgetter(1), reverse=True)
+        PageRank[doc] = prDoc
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    return PageRank
 
 
 # --------------------------------------------------------------------#
@@ -492,7 +422,7 @@ trainData = train.data[:10]
 
 # Get relative path to documents
 datasetPath = os.path.dirname(os.path.abspath(__file__)) + "\\documents\\";
-
+trainPath = os.path.dirname(os.path.abspath(__file__)) + "\\maui-semeval2010-train\\";
 
 # Get all documents in "documents" directory into fileList
 # Import documents into fileList
@@ -515,7 +445,88 @@ inputCont = inputCont.decode("unicode_escape")
 inputData={}
 inputData["Nihilism Wikipedia"]= inputCont
 
-keyphrases = extractKeyphrases(fileList, fileList)
+keyphrasesPageRanks = extractKeyphrases(fileList, fileList)
+
+
+
+
+#Get known Kephrases for each doc
+KnownKeyphrases = {}
+for docName in fileList:
+
+    docKeyphrases = []
+    knownKeyphrases=[]
+
+    # Get all document's known keyphrases in "keys" directory into knownKeyphrases
+    # Import documents into fileList0
+    fKeys = open(datasetPath + docName + ".key", 'r')
+    docKeyphrases = fKeys.read()
+    fKeys.close()
+
+    knownKeyphrases = docKeyphrases.splitlines()
+    knownKeyphrases = [x.decode("unicode_escape") for x in knownKeyphrases]
+
+    #predictedKeyphrases = calculatedKeyScores[nDocs].keys()
+    # print knownKeyphrases
+    # print predictedKeyphrases
+
+    # Performance metrics - Calculate precision, recall, F1 scores and mean average precision
+    #allPrecisions += [calc_precision_recall_f1_score(docName, knownKeyphrases, predictedKeyphrases)[0]]
+
+    KnownKeyphrases[docName]= knownKeyphrases
+
+
+
+for doc in keyphrasesPageRanks:
+    print "Doc: " + doc
+    print " --- Page Rank ---"
+
+    for type in keyphrasesPageRanks[doc]:
+        print type
+
+        print keyphrasesPageRanks[doc][type]
+
+    print " --- Known keys ---"
+    print KnownKeyphrases[doc]
+
+    print "\n"
+
+
+## PRECISIONS[TYPE RANK][[list scores][avg]]
+
+Scores={}
+Scores['All']=[]
+Precisions={}
+Precisions['ORIGINAL']= Scores
+Precisions['IMPROVED - SENTENCE PRIOR AND CO-OCCURENCE WEIGHT']= Scores
+Precisions['IMPROVED - BM25 PRIOR AND CO-OCCURENCE WEIGHT']= Scores
+
+for doc in keyphrasesPageRanks:
+    #docScores={}
+
+    for typePageRank in keyphrasesPageRanks[doc]:
+
+        # scores={}
+        # scores = calc_precision_recall_f1_score(doc, keyphrasesPageRanks[doc][typePageRank].keys(), KnownKeyphrases[doc] )
+        extracted=[]
+        for elem in keyphrasesPageRanks[doc][typePageRank]:
+            extracted+=[elem[0]]
+
+        #print extracted
+
+        precision= precisonCalc( extracted, KnownKeyphrases[doc] )
+
+        Precisions[typePageRank]['All']+=[precision]
+
+
+
+for type in Precisions:
+    print type
+    print "Values: " + str(Precisions[type]['All'])
+    Precisions[type]['Mean Avg Precision'] = calc_mean_avg_precision(Precisions[type]['All'])
+    print "Mean Avg Precision: " + str(Precisions[type]['Mean Avg Precision'])
+
+    print "\n"
 
 
 
